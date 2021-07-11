6a54528a4bea16d381a8a11f54ce6821a44a56d1
Oleg Grytsynevych <oleg.gritsinevich@gmail.com>
2021-07-11 12:27:04 +0200

    Added a "monolith" (SampleServer).

---
+++ SampleServer/SampleServer/Program.cs
@@ -0,0 +1,39 @@
+﻿using Nancy;
+using Nancy.Hosting.Self;
+using System;
+using System.Threading;
+
+namespace SampleServer
+{
+
+    public class VersionModule : NancyModule
+    {
+        public VersionModule()
+        {
+            Get("/", parameters => "Version 0.1");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var nancyHost = new NancyHost(new Uri("http://localhost:8888/")))
+            {
+                nancyHost.Start();
+
+                Console.WriteLine("Nancy now listening - navigating to http://localhost:8888/. Press enter to stop");
+
+                var timer = new Timer(savePolicy, null, 0, 3000);
+                Console.ReadKey();
+                timer.Dispose();
+            }
+
+            Console.WriteLine("Stopped. Good bye!");
+        }
+
+        private static void savePolicy(object state)
+        {
+            Console.WriteLine("Saving policy");
+        }
+    }
+}
